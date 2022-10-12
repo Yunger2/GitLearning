e5269a3a721cf139325a7399c4e491b35fd05a37
knocker01 <knocker01@qq.com>
2022-10-12 16:10:30 +0800

    my second commit

---
+++ build/CMakeFiles/my_cmake_debug.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../bin/debug/libmy_cmake_debug.dll.a"
+  "../bin/debug/my_cmake_debug.exe"
+  "../bin/debug/my_cmake_debug.exe.manifest"
+  "../bin/debug/my_cmake_debug.pdb"
+  "CMakeFiles/my_cmake_debug.dir/src/knocker-01.obj"
+  "CMakeFiles/my_cmake_debug.dir/src/swap.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_cmake_debug.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
